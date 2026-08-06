e44910137ceb7a6fe2483b42ca6fd36162416792
Anass El Bakkali <yb4994li@gmail.com>
2026-08-07 00:19:40 +0100

    fix(performance): orgenize charts and improve UI(1)

---
+++ client/src/components/performance/chartPalette.js
@@ -0,0 +1,2 @@
+export const PERFORMANCE_SERIES_COLORS=['#c7f36b','#60a5fa','#94a3b8','#fbbf24','#a78bfa','#2dd4bf'];
+export const seriesColor=(index)=>PERFORMANCE_SERIES_COLORS[index%PERFORMANCE_SERIES_COLORS.length];
